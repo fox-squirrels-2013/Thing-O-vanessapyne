d29755c948be414f2a4baada0e3dbaa6d68b7c7b
vanessa <vipyne@gmail.com>
2013-11-01 01:54:57 -0700

    working on restful

--- app.rb
+++ app.rb
@@ -5,27 +5,33 @@ require_relative './models/saab.rb'
 ActiveRecord::Base.establish_connection(adapter: 'postgresql',
   database: 'saab' )
 
-
 get '/' do
   erb :index
 end
 
 get '/allsaabs' do
+  @view = Saab.all
   erb :allsaabs
 end
 
 post '/allsaabs' do
   puts params
-  @view = Saab.all
   if params[:deleted]
     Saab.find(params[:deleted]).destroy
-  else
+  end
+  if params[:model]
     @model = params[:model]
     @color = params[:color]
     @year = params[:year]
     Saab.create(model: @model, color: @color, year: @year)
   end
-    erb :allsaabs
+    redirect '/allsaabs'
+end
+
+get '/allsaabs/:id' do
+  @one = Saab.find(params[:id])
+  p @one.id
+  erb :one_car
 end
 
 
